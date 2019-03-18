84f36457bfb57983166a84eb6938f79d895af7b0
bulatovav <bulatovav@vdcom.ru>
2019-03-18 16:41:29 +0400

    Created model of repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div class="bg-success p-a-1 text-xs-center">
-                 This is SportsStore
-             </div>`
+  template: '<store></store>'
 })
 export class AppComponent { }
